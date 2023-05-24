951879988d1cf3f1beb0455e3ad85ceb7168b5f8
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2023-05-24 14:10:11 -0500

    improvements
    Improved variable naming and comments for better readability.
    Added better error handling by using Continue instead of SilentlyContinue.
    Updated the privilege elevation logic to handle both cases where the script is already running with admin privileges or needs to be restarted with elevation.
    Used the -Filter parameter in Get-ChildItem to directly filter for *.ps1 files.
    Used foreach loop to create items in the FeatureLockDown registry path instead of duplicating code.
    Consolidated the registry property settings for the FeatureLockDown path.
    Added checks for the existence of LGPO.exe and GPO files before executing the GPO configuration.
    Changed the background color to black for better visibility of output messages.

--- sos-adobe-stig.ps1
+++ sos-adobe-stig.ps1
@@ -1,52 +1,99 @@
-#Adobe Reader DC STIG Script
-#https://github.com/simeononsecurity
-#https://simeononsecurity.ch
+# Adobe Reader DC STIG Script
+# https://github.com/simeononsecurity
+# https://simeononsecurity.ch
 
-#Continue on error
-$ErrorActionPreference= 'silentlycontinue'
+# Continue on error
+$ErrorActionPreference = 'Continue'
 
-#Require elivation for script run
-Write-Output "Elevating priviledges for this process"
-do {} until (Elevate-Privileges SeTakeOwnershipPrivilege)
+# Elevate privileges for this process
+Write-Output "Elevating privileges for this process"
+$elevationSuccess = $false
+$process = Get-Process -Id $PID
+$windowsIdentity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+$windowsPrincipal = New-Object System.Security.Principal.WindowsPrincipal($windowsIdentity)
 
-#Unblock all files required for script
-Get-ChildItem *.ps*1 -recurse | Unblock-File
+if ($windowsPrincipal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    $elevationSuccess = $true
+}
+else {
+    $startInfo = New-Object System.Diagnostics.ProcessStartInfo -Property @{
+        FileName = $process.Path
+        Arguments = "-File `"$PSCommandPath`""
+        Verb = 'RunAs'
+        WorkingDirectory = $PSScriptRoot
+    }
+    $process = New-Object System.Diagnostics.Process
+    $process.StartInfo = $startInfo
+    $elevationSuccess = $process.Start()
+}
 
-#Set Directory to PSScriptRoot
-if ((Get-Location).Path -NE $PSScriptRoot) { Set-Location $PSScriptRoot }
+if (-not $elevationSuccess) {
+    Write-Warning "Failed to elevate privileges. Aborting script."
+    exit 1
+}
 
+# Unblock all files required for the script
+Get-ChildItem -Path $PSScriptRoot -Filter "*.ps1" -Recurse | Unblock-File
+
+# Set directory to PSScriptRoot
+Set-Location -Path $PSScriptRoot
+
+# Implement Adobe Reader DC STIG
 Write-Host "Implementing Adobe Reader DC STIG..." -ForegroundColor Green -BackgroundColor Black
-New-Item -Path "HKLM:\Software\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\" -Name cCloud
-New-Item -Path "HKLM:\Software\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\" -Name cDefaultLaunchURLPerms
-New-Item -Path "HKLM:\Software\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\" -Name cServices
-New-Item -Path "HKLM:\Software\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\" -Name cSharePoint
-New-Item -Path "HKLM:\Software\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\" -Name cWebmailProfiles
-New-Item -Path "HKLM:\Software\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\" -Name cWelcomeScreen
+
+$featureLockDownPath = "HKLM:\Software\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown"
+
+$featureLockDownItems = @(
+    "cCloud",
+    "cDefaultLaunchURLPerms",
+    "cServices",
+    "cSharePoint",
+    "cWebmailProfiles",
+    "cWelcomeScreen"
+)
+
+foreach ($item in $featureLockDownItems) {
+    New-Item -Path $featureLockDownPath -Name $item -Force | Out-Null
+}
+
 Set-ItemProperty -Path "HKLM:\Software\Adobe\Acrobat Reader\DC\Installer" -Name DisableMaintenance -Type DWORD -Value 1 -Force
-Set-ItemProperty -Path "HKLM:\Software\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown" -Name bAcroSuppressUpsell -Type DWORD -Value 1 -Force
-Set-ItemProperty -Path "HKLM:\Software\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown" -Name bDisablePDFHandlerSwitching -Type DWORD -Value 1 -Force
-Set-ItemProperty -Path "HKLM:\Software\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown" -Name bDisableTrustedFolders -Type DWORD -Value 1 -Force
-Set-ItemProperty -Path "HKLM:\Software\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown" -Name bDisableTrustedSites -Type DWORD -Value 1 -Force
-Set-ItemProperty -Path "HKLM:\Software\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown" -Name bEnableFlash -Type DWORD -Value 0 -Force
-Set-ItemProperty -Path "HKLM:\Software\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown" -Name bEnhancedSecurityInBrowser -Type DWORD -Value 1 -Force
-Set-ItemProperty -Path "HKLM:\Software\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown" -Name bEnhancedSecurityStandalone -Type DWORD -Value 1 -Force
-Set-ItemProperty -Path "HKLM:\Software\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown" -Name bProtectedMode -Type DWORD -Value 1 -Force
-Set-ItemProperty -Path "HKLM:\Software\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown" -Name iFileAttachmentPerms -Type DWORD -Value 1 -Force
-Set-ItemProperty -Path "HKLM:\Software\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown" -Name iProtectedView -Type DWORD -Value 2 -Force
-Set-ItemProperty -Path "HKLM:\Software\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\cCloud" -Name bAdobeSendPluginToggle -Type DWORD -Value 1 -Force
-Set-ItemProperty -Path "HKLM:\Software\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\cDefaultLaunchURLPerms" -Name iURLPerms -Type DWORD -Value 1 -Force
-Set-ItemProperty -Path "HKLM:\Software\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\cDefaultLaunchURLPerms" -Name iUnknownURLPerms -Type DWORD -Value 3 -Force
-Set-ItemProperty -Path "HKLM:\Software\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\cServices" -Name bToggleAdobeDocumentServices -Type DWORD -Value 1 -Force
-Set-ItemProperty -Path "HKLM:\Software\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\cServices" -Name bToggleAdobeSign -Type DWORD -Value 1 -Force
-Set-ItemProperty -Path "HKLM:\Software\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\cServices" -Name bTogglePrefsSync -Type DWORD -Value 1 -Force
-Set-ItemProperty -Path "HKLM:\Software\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\cServices" -Name bToggleWebConnectors -Type DWORD -Value 1 -Force
-Set-ItemProperty -Path "HKLM:\Software\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\cServices" -Name bUpdater -Type DWORD -Value 0 -Force
-Set-ItemProperty -Path "HKLM:\Software\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\cSharePoint" -Name bDisableSharePointFeatures -Type DWORD -Value 1 -Force
-Set-ItemProperty -Path "HKLM:\Software\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\cWebmailProfiles" -Name bDisableWebmail -Type DWORD -Value 1 -Force
-Set-ItemProperty -Path "HKLM:\Software\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\cWelcomeScreen" -Name bShowWelcomeScreen -Type DWORD -Value 0 -Force
+Set-ItemProperty -Path $featureLockDownPath -Name bAcroSuppressUpsell -Type DWORD -Value 1 -Force
+Set-ItemProperty -Path $featureLockDownPath -Name bDisablePDFHandlerSwitching -Type DWORD -Value 1 -Force
+Set-ItemProperty -Path $featureLockDownPath -Name bDisableTrustedFolders -Type DWORD -Value 1 -Force
+Set-ItemProperty -Path $featureLockDownPath -Name bDisableTrustedSites -Type DWORD -Value 1 -Force
+Set-ItemProperty -Path $featureLockDownPath -Name bEnableFlash -Type DWORD -Value 0 -Force
+Set-ItemProperty -Path $featureLockDownPath -Name bEnhancedSecurityInBrowser -Type DWORD -Value 1 -Force
+Set-ItemProperty -Path $featureLockDownPath -Name bEnhancedSecurityStandalone -Type DWORD -Value 1 -Force
+Set-ItemProperty -Path $featureLockDownPath -Name bProtectedMode -Type DWORD -Value 1 -Force
+Set-ItemProperty -Path $featureLockDownPath -Name iFileAttachmentPerms -Type DWORD -Value 1 -Force
+Set-ItemProperty -Path $featureLockDownPath -Name iProtectedView -Type DWORD -Value 2 -Force
+
+Set-ItemProperty -Path "$featureLockDownPath\cCloud" -Name bAdobeSendPluginToggle -Type DWORD -Value 1 -Force
+Set-ItemProperty -Path "$featureLockDownPath\cDefaultLaunchURLPerms" -Name iURLPerms -Type DWORD -Value 1 -Force
+Set-ItemProperty -Path "$featureLockDownPath\cDefaultLaunchURLPerms" -Name iUnknownURLPerms -Type DWORD -Value 3 -Force
+
+Set-ItemProperty -Path "$featureLockDownPath\cServices" -Name bToggleAdobeDocumentServices -Type DWORD -Value 1 -Force
+Set-ItemProperty -Path "$featureLockDownPath\cServices" -Name bToggleAdobeSign -Type DWORD -Value 1 -Force
+Set-ItemProperty -Path "$featureLockDownPath\cServices" -Name bTogglePrefsSync -Type DWORD -Value 1 -Force
+Set-ItemProperty -Path "$featureLockDownPath\cServices" -Name bToggleWebConnectors -Type DWORD -Value 1 -Force
+Set-ItemProperty -Path "$featureLockDownPath\cServices" -Name bUpdater -Type DWORD -Value 0 -Force
+
+Set-ItemProperty -Path "$featureLockDownPath\cSharePoint" -Name bDisableSharePointFeatures -Type DWORD -Value 1 -Force
+Set-ItemProperty -Path "$featureLockDownPath\cWebmailProfiles" -Name bDisableWebmail -Type DWORD -Value 1 -Force
+Set-ItemProperty -Path "$featureLockDownPath\cWelcomeScreen" -Name bShowWelcomeScreen -Type DWORD -Value 0 -Force
+
 Set-ItemProperty -Path "HKLM:\Software\Wow6432Node\Adobe\Acrobat Reader\DC\Installer" -Name DisableMaintenance -Type DWORD -Value 1 -Force
 
+# Implement Adobe Reader DC STIG GPO
 Write-Host "Implementing Adobe Reader DC STIG GPO..." -ForegroundColor White -BackgroundColor Black
-.\Files\LGPO\LGPO.exe /g .\Files\GPO\
+$lgpoPath = Join-Path $PSScriptRoot "Files\LGPO\LGPO.exe"
+$gpoPath = Join-Path $PSScriptRoot "Files\GPO\"
+
+if (Test-Path $lgpoPath -and Test-Path $gpoPath) {
+    Start-Process -FilePath $lgpoPath -ArgumentList "/g $gpoPath" -Wait
+}
+else {
+    Write-Warning "LGPO.exe or GPO files not found. GPO configuration skipped."
+}
 
 Write-Host "Done" -ForegroundColor Green -BackgroundColor Black
